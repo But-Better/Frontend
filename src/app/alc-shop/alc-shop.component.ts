8a1b5f6acdccdb656214ad4b2519f320af1bb941
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2022-03-17 19:15:17 +0100

    add component for the alcohol shop displaying test data in table (render over app.component.html)

---
+++ src/app/alc-shop/alc-shop.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Alcohol } from '../model/alcohol';
+
+@Component({
+  selector: 'app-alc-shop',
+  templateUrl: './alc-shop.component.html',
+  styleUrls: ['./alc-shop.component.css']
+})
+export class AlcShopComponent implements OnInit {
+
+  alcoholList: Alcohol[] = [
+    {id: '1234', type: 'Bier', name: 'Zwickl', price: 1.50, alcPercentage: 5.0, amount: 100, rating: 5, ageRestriction: 16, fairtrade: true, bio: false, countryOfOrigin: 'Germany'},
+    {id: '12345', type: 'Wein', name: 'Der gute Rote', price: 5.50, alcPercentage: 15.0, amount: 25, rating: 10, ageRestriction: 16, fairtrade: true, bio: true, countryOfOrigin: 'Germany'}
+];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

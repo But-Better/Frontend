de4abff01a75737d675095b27a82af93e2ca779b
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2022-03-17 18:21:32 +0100

    add alcohol model

---
+++ src/app/model/alcohol.ts
@@ -0,0 +1,13 @@
+export class Alcohol{
+    id!: string;
+    type!: string;
+    name!: string;
+    price!: number;
+    alcPercentage!: number;
+    amount!: number;
+    rating!: number;
+    ageRestriction!: number;
+    fairtrade!: boolean;
+    bio!: boolean;
+    countryOfOrigin!: string;
+}

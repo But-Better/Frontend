acb240ed6d17337db690afdb26c15c9761ae20f0
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2022-03-17 19:52:17 +0100

    add route for the shop site and remove home button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlcShopComponent } from './alc-shop/alc-shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'shop', component: AlcShopComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

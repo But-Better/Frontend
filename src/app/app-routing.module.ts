e67c3e690c1cd6e4b8defa791422100bfc5bfe88
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2022-03-20 10:34:25 +0100

    add new routes for input form and find by id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AlcShopComponent } from './alc-shop/alc-shop.component';
+import { AlcoholFormComponent } from './alcohol-form/alcohol-form.component';
+import { FoundAlcComponent } from './found-alc/found-alc.component';
 
 const routes: Routes = [
   { path: 'shop', component: AlcShopComponent },
+  { path: 'newAlc', component: AlcoholFormComponent},
+  { path: 'foundAlc/:id', component: FoundAlcComponent}
 ];
 
 @NgModule({

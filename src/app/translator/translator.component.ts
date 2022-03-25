4e6b0f2b85c3e2f45b601d29a33198c5920ddbde
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2022-03-25 16:02:29 +0100

    add language analyser to translator

--- src/app/translator/translator.component.ts
+++ src/app/translator/translator.component.ts
@@ -11,6 +11,7 @@ export class TranslatorComponent implements OnInit {
   private textInput:string = '';
   private languageInput:string='';
   translatedText:string = '';
+  analysedLanguage:string = '';
 
 
 
@@ -27,6 +28,44 @@ export class TranslatorComponent implements OnInit {
 
     this.translatorService.translate(this.textInput,this.languageInput).subscribe(translation => this.translatedText = translation);
     console.log("recieved this as translation: " + this.translatedText);
+  }
+
+  getLanguage(text:string){
+    this.textInput = text;
+    console.log("got this as text input: " + this.textInput);
+
+    this.translatorService.analyseLanguage(text).subscribe(language => this.analysedLanguage = this.shortToLongLanguage(language));
+    console.log("recieved this as language: " + this.analysedLanguage);
+  }
+
+  private shortToLongLanguage(shortLang:string): string{
+    switch(shortLang){
+      case '"DE"': return 'German';
+      case '"EN"': return 'English';
+      case '"BG"': return 'Bulgarian';
+      case '"CS"': return 'Czech';
+      case '"DA"': return 'Danish';
+      case '"EL"': return 'Greek';
+      case '"ES"': return 'Spanish';
+      case '"ET"': return 'Estonian';
+      case '"FI"': return 'Finish';
+      case '"FR"': return 'French';
+      case '"HU"': return 'Hungarian';
+      case '"IT"': return 'Italian';
+      case '"JA"': return 'Japanese';
+      case '"LT"': return 'Lithuanian';
+      case '"LV"': return 'Latvian';
+      case '"NL"': return 'Dutch';
+      case '"PL"': return 'Polish';
+      case '"PT"': return 'Portugese';
+      case '"RO"': return 'Romanian';
+      case '"RU"': return 'Russian';
+      case '"SK"': return 'Slovak';
+      case '"SL"': return 'Slovenian';
+      case '"SV"': return 'Swedish';
+      case '"ZH"': return 'Chinese';
+      default:return 'Unknown';
+    }
   }
 
 }

03eb138fb0183c9cf37f414ac31ec15d132c23e0
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2022-03-25 11:40:17 +0100

    add translator component with navbar link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { AlcoholFormComponent } from './alcohol-form/alcohol-form.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { FoundAlcComponent } from './found-alc/found-alc.component';
 import { ProductinfoComponent } from './productinfo/productinfo.component';
+import { TranslatorComponent } from './translator/translator.component';
 
 const routes: Routes = [
   { path: 'shop', component: AlcShopComponent },
   { path: 'newAlc', component: AlcoholFormComponent},
   { path: 'foundAlc/:id', component: FoundAlcComponent},
   { path: 'prodInfo', component: ProductinfoComponent},
-  { path: 'calculator', component: CalculatorComponent}
+  { path: 'calculator', component: CalculatorComponent},
+  { path: 'translator', component: TranslatorComponent}
 
 ];
 
